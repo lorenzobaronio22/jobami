3f4f538acaea9cd92abfca118f2cc00ab58e33da
Lorenzo Baronio <lbaronio@xdatanet.com>
2025-06-14 16:27:06 +0200

    feat: add index page and installed module nuxt-auth-utils

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: ['nuxt-auth-utils'],
+  runtimeConfig: {
+    session: {
+      password: process.env.NUXT_SESSION_PASSWORD || '',
+      maxAge: 60 * 60 * 24 * 7 // 1 week
+    }
+  }
 })
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
-
 </script>
 
 <template>
   <div>
-    <NuxtPage />
+    <h1>Hello World</h1>
+    <p>This is the index page.</p>
   </div>
 </template>
+
