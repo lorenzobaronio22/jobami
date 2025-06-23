a362ca1a8d2abb3cbd3ba0fb1be03023030d121d
Lorenzo Baronio <lbaronio@xdatanet.com>
2025-06-23 09:38:53 +0200

    feat: add console logs for API_URL in database setup and teardown tests

--- tests/global.setup.ts
+++ tests/global.setup.ts
@@ -3,6 +3,7 @@ import { createClient } from '@supabase/supabase-js'
 import { API_URL, SERVICE_ROLE_KEY, USER_1_EMAIL, USER_1_ID, USER_1_PASSWORD } from './global.data';
 
 test('create new database', async ({ }) => {
+  console.log('Creating new database API_URL: ', API_URL);
   const adminSupabase = createClient(API_URL, SERVICE_ROLE_KEY)
   await adminSupabase.auth.admin.createUser({
     id: USER_1_ID,
--- tests/global.teardown.ts
+++ tests/global.teardown.ts
@@ -3,6 +3,7 @@ import { createClient } from '@supabase/supabase-js'
 import { API_URL, SERVICE_ROLE_KEY, USER_1_ID } from './global.data';
 
 test('delete database', async ({ }) => {
+  console.log('delete database API_URL: ', API_URL);
   const adminSupabase = createClient(API_URL, SERVICE_ROLE_KEY)
   await adminSupabase.auth.admin.deleteUser(USER_1_ID)
 
